8d9e912292c17eaa47d8606dc222523c0351b7d2
JaeHo Song <thruthesky@gmail.com>
2014-04-10 20:26:14 +0900

    commenting on sample widget

--- sample-widget.php
+++ sample-widget.php
@@ -1,6 +1,8 @@
 <?php
 /**
  *  @file sample-widget.php
+ *  @short This is sample widget. copy this and re-make your own.
+ *  본 위젯은 견본용입니다. 이 위젯을 복사해서 사용하시면 됩니다.
  */
 ?>
 <?=$widget_config['html']?>
